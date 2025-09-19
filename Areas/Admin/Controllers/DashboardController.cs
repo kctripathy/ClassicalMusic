f1a531c51d88afce841f11f70b85042f1709b501
Kishor Tripathy <kishor.tripathy@version1.com>
2025-09-19 17:42:24 +0530

    Login issue fixed and created an area for admin

---
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClassicalMusicApp.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class DashboardController : Controller
+    {
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -5,19 +5,19 @@
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ClassicalMusicApp.Models.ApplicationDbContext>
-    {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = false;
-        }
+    //internal sealed class Configuration : DbMigrationsConfiguration<ClassicalMusicApp.Models.ApplicationDbContext>
+    //{
+    //    public Configuration()
+    //    {
+    //        AutomaticMigrationsEnabled = false;
+    //    }
 
-        protected override void Seed(ClassicalMusicApp.Models.ApplicationDbContext context)
-        {
-            //  This method will be called after migrating to the latest version.
+    //    protected override void Seed(ClassicalMusicApp.Models.ApplicationDbContext context)
+    //    {
+    //        //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
-        }
-    }
+    //        //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+    //        //  to avoid creating duplicate seed data.
+    //    }
+    //}
 }
