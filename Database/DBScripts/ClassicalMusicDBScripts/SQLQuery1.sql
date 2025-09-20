c49a20798eaf93d424b9110e72cb84fd7f5bd287
Kishor Tripathy <kishor.tripathy@version1.com>
2025-09-20 11:10:24 +0530

    added db script for aspnet

---
+++ Database/DBScripts/ClassicalMusicDBScripts/SQLQuery1.sql
@@ -0,0 +1,13 @@
+select * from Raaga
+
+select * from Composition
+
+update Composition set IsActive = 1 where id < 11
+
+update raaga set RaagaDesc='One of the most popular ragas in Hindustani music, Raag Yaman evokes peace, love, and devotion, and is often the first raga taught to beginners.' where ID=1
+
+UPDATE Raaga SET Aroh=N'? R G ? D N ?', Avroh=N'? N D P ? G R S',Pakad=N'? R G, G ?, P D N D P, ? G R, ? R S',Vadi=N'G',Samvadi=N'N' WHERE ID=1
+
+UPDATE Raaga SET ThaatID=2 WHERE ID=1
+
+select * from Artist
